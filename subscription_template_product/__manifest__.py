1a652b6b5bfd90131f013d57703c62bc5fcf3f3f
mikevhe18 <viriyananda.michael@gmail.com>
2022-01-06 10:05:48 +0700

    [subscription_template_product] 11.0.1.0.0 Task: T/22/005386

---
+++ subscription_template_product/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2022 PT. Simetri Sinergi Indonesia.
+# Copyright 2022 OpenSynergy Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "Subscription Template Product",
+    "version": "11.0.1.0.0",
+    "license": "LGPL-3",
+    "category": "Sales",
+    "website": "https://simetri-sinergi.id",
+    "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
+    "depends": [
+        "sale_subscription",
+    ],
+    "data": [
+        "views/sale_subscription_template_views.xml",
+    ],
+    "installable": True,
+    "auto_install": False,
+}
