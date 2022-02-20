f91054598882ecb7f6f44185e61f5169e6202f2e
Andhitia Rama <andhitia.r@gmail.com>
2022-02-20 19:32:12 +0700

    [subscription_hierarchy] 11.0.1.0.0

---
+++ subscription_hierarchy/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2022 PT. Simetri Sinergi Indonesia.
+# Copyright 2022 OpenSynergy Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "Subscription Hierarchy",
+    "version": "11.0.1.0.0",
+    "license": "LGPL-3",
+    "category": "Sales",
+    "website": "https://simetri-sinergi.id",
+    "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
+    "depends": [
+        "subscription_workflow_policy",
+    ],
+    "data": [
+        "views/sale_subscription_views.xml",
+    ],
+    "installable": True,
+    "auto_install": False,
+}
