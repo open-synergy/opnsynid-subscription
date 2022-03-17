16379ad3927b28b4faa07d39d1dc93379bfb5ed2
Andhitia Rama <andhitia.r@gmail.com>
2022-03-17 14:04:09 +0700

    [subscription_person_in_charge] 11.0.1.0.0

---
+++ subscription_person_in_charge/models/sale_subscription.py
@@ -0,0 +1,47 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+
+from odoo import api, fields, models
+
+
+class SaleSubscription(models.Model):
+    _name = "sale.subscription"
+    _inherit = "sale.subscription"
+
+    @api.depends(
+        "partner_id",
+    )
+    def _compute_allowed_pic_ids(self):
+        obj_partner = self.env["res.partner"]
+        for record in self:
+            result = []
+            if record.partner_id:
+                criteria = [
+                    (
+                        "commercial_partner_id",
+                        "=",
+                        record.partner_id.commercial_partner_id.id,
+                    ),
+                    ("is_company", "=", False),
+                ]
+                result = obj_partner.search(criteria).ids
+            record.allowed_pic_ids = result
+
+    allowed_pic_ids = fields.Many2many(
+        string="Allowed Invoice To",
+        comodel_name="res.partner",
+        compute="_compute_allowed_pic_ids",
+    )
+
+    pic_id = fields.Many2one(
+        string="Person in Charge",
+        comodel_name="res.partner",
+    )
+
+    @api.onchange(
+        "partner_id",
+    )
+    def onchange_pic_id(self):
+        self.pic_id = False
