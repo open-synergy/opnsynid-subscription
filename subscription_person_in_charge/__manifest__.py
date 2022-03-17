306eadec985d88fa7445b9d7733b70078eafb85a
Andhitia Rama <andhitia.r@gmail.com>
2022-03-17 14:04:09 +0700

    [subscription_person_in_charge] 11.0.1.0.0

---
+++ subscription_person_in_charge/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2022 PT. Simetri Sinergi Indonesia.
+# Copyright 2022 OpenSynergy Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "Subscription Person In Charge",
+    "version": "11.0.1.0.0",
+    "license": "LGPL-3",
+    "category": "Sales",
+    "website": "https://simetri-sinergi.id",
+    "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
+    "depends": [
+        "sale_subscription",
+    ],
+    "data": [
+        "views/sale_subscription_views.xml",
+    ],
+    "installable": True,
+    "auto_install": False,
+}
