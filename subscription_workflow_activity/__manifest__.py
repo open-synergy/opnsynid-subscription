dfbcc81cf4409e250ce4bcffdbdf7f9da31a42a0
Andhitia Rama <andhitia.r@gmail.com>
2022-05-17 17:22:04 +0700

    [subscription_workflow_activity] 11.0.1.0.0

---
+++ subscription_workflow_activity/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2022 PT. Simetri Sinergi Indonesia.
+# Copyright 2022 OpenSynergy Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "Subscription Workflow Activity",
+    "version": "11.0.1.0.0",
+    "license": "LGPL-3",
+    "category": "Sales",
+    "website": "https://simetri-sinergi.id",
+    "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
+    "depends": [
+        "subscription_workflow_policy",
+    ],
+    "data": [],
+    "installable": True,
+    "auto_install": False,
+}
