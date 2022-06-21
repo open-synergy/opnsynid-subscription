d08aa4b5bda906300fce3e13e9bff06a16d97ba5
Andhitia Rama <andhitia.r@gmail.com>
2022-06-21 12:33:49 +0700

    [subscription_auto_close] 11.0.1.0.0

---
+++ subscription_auto_close/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2022 PT. Simetri Sinergi Indonesia.
+# Copyright 2022 OpenSynergy Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "Subscription Auto Close",
+    "version": "11.0.1.0.0",
+    "license": "LGPL-3",
+    "website": "https://simetri-sinergi.id",
+    "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
+    "depends": [
+        "subscription_date_end",
+    ],
+    "data": [
+        "views/sale_subscription_template_views.xml",
+    ],
+    "installable": True,
+}
